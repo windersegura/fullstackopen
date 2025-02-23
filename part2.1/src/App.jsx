1dd1ecdc436d08d40c062f45bac622a41c3f73c1
winders98 <segurawinder@gmail.com>
2025-02-23 17:17:21 -0600

    react states

--- part2.1/src/App.jsx
+++ part2.1/src/App.jsx
@@ -1,25 +1,49 @@
-const Hello = (props) => {
+import {useState} from 'react';
 
+const Display = (props) => {
   return(
-    <div>
-      <p>
-        Hello {props.name}, you are {props.age} years old!
-      </p>
-    </div>
+    <div>{props.counter}</div>
   )
+}
 
+const Button = (props) => {
+  return(
+    <button onClick={props.onClick}>
+      {props.text}
+    </button>
+  )
 }
 
 
+
 function App() {
-  const name = 'Peter';
-  const age = 27;
+  
+  const [counter, setCounter] = useState(0)
+  console.log('rendering with counter value ', counter );
+  
+
+  const increaseByOne = () => {
+    console.log('increasing counter by one value before', counter);
+    
+    setCounter(counter + 1)
+  }
+  const decreaseByOne = () => {
+    console.log('decreasing counter by one value before', counter);
+    
+    setCounter(counter - 1)
+  }
+  const resetCounter = () => {
+    console.log('resetting to zero de value before', counter);
+    
+    setCounter(0)
+  }
 
   return (
     <div>
-      <h1>Greatings!!</h1>
-      <Hello name='Maria' age={10 + 20}/>
-      <Hello name={name} age={age}/>
+      <Display counter={counter}/>
+      <Button onClick={increaseByOne} text='plus'/>
+      <Button onClick={resetCounter} text='zero'/>
+      <Button onClick={decreaseByOne} text='minus'/>
     </div>
   )
 }
