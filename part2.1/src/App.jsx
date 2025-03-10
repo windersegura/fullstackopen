46ee5b697b620534c865380a4531f57775dd99ce
winders98 <segurawinder@gmail.com>
2025-03-09 21:49:19 -0600

    working with forms

--- part2.1/src/App.jsx
+++ part2.1/src/App.jsx
@@ -1,54 +1,44 @@
 import {useState} from 'react';
+import Note from './components/Note';
 
 
-const History = (props) => {
-  if(props.allClicks.length === 0){
-    return (
-      <div>
-        the app is used by pressing the buttons
-      </div>
-    )
-  }
-  return (
-    <div>
-      button press history: {props.allClicks.join(' ')}
-    </div>
+
+function App(props) {
+  const [notes, setNotes] = useState(props.notes);
+  const [newNote, setNewNote] = useState(
+    'a new note...'
   )
-}
 
-const Button = ({handleClick, text}) => (
-  <button onClick={handleClick}>
-    {text}
-  </button>
-)
+  const addNote = (event) => {
+    event.preventDefault();
+    const noteObject = {
+      content: newNote,
+      important: Math.random() < 0.5,
+      id: notes.length + 1
+     };
 
-function App() {
-
-  const [left,setLeft] = useState(0);
-  const [right,setRight] = useState(0);
-  const [allClicks, setAll] = useState([]);
-  const [total, setTotal] = useState(0)
-
-  const handleLeftClick = () => {
-    setAll(allClicks.concat('L'));
-    const updateLeft = left + 1;
-    setLeft(updateLeft);
-    setTotal(updateLeft + right);
+     setNotes(notes.concat(noteObject));
+     setNewNote('');
+    
   }
 
-  const handleRightClick = () => {
-    setAll(allClicks.concat('R'));
-    setRight(right + 1);
-    setTotal(left + right);
+  const handleNoteChange = (event) =>{
+    console.log(event.target.value);
+    setNewNote(event.target.value);
+    
   }
 
   return (
     <div>
-      {left}
-      <Button handleClick={handleLeftClick} text= 'left'/>
-      <Button handleClick={handleRightClick} text='right'/>
-      {right}
-      <History allClicks={allClicks} />
+      <h1>Notes</h1>
+      <ul>
+        {notes.map( note => <Note key={note.id} note={note} />)}
+      </ul>
+
+      <form onSubmit={addNote}>
+        <input value={newNote} onChange={handleNoteChange}/>
+        <button type='submit'>save</button>
+      </form>
     </div>
   )
 }
